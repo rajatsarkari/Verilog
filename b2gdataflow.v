bb0206a249f6497aae8fc34ffe67a24ce90d8200
rajatsarkari <rajatsarkari777@gmail.com>
2018-04-03 02:16:20 +0530

    Dataflow Addition commit

--- b2gdataflow.v
+++ b2gdataflow.v
@@ -1,8 +1,16 @@
 module b2gdataflow(bin, gray);
 	parameter N = 4;
-	input [N-1:0] gray, bin;
+	input [N-1:0] gray;
+	output [N-1:0] bin;
 	assign gray[N-1] = bin[N-1];
 	assign gray[N-2:0] = (bin[N-1:1]^bin[N-2:0]); 
+endmodule
+module g2bdataflow(bin, gray);
+	parameter N = 4;
+	output [N-1:0] gray;
+	input [N-1:0] bin;
+	assign bin[N-1] = gray[N-1];
+	assign bin[N-2:0] = (gray[N-2:0]^bin[N-1:1]); 
 endmodule
 module b2g_test;
 	reg [3:0] bin;
@@ -11,5 +19,14 @@ module b2g_test;
 	b2gdataflow uut(.*);
 	initial begin
 		#2 bin = 4'b0101;
+	end
+endmodule
+module g2b_test;
+	wire [3:0] bin;
+	reg [3:0] gray;
+
+	g2bdataflow uut(.*);
+	initial begin
+		#2 gray = 4'b1001;
 	end
 endmodule
